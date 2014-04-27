9b1136f4996c5ce7923c56db3e41c263c4cd9ae3
GIT Admin <yasmindemiral@gmail.com>
2014-04-27 14:16:03 +0300

    layoutlar arası geçiş

--- app/src/main/java/com/bebek/takip/MainActivity.java
+++ app/src/main/java/com/bebek/takip/MainActivity.java
@@ -1,37 +1,59 @@
 package com.bebek.takip;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
+import android.content.Intent;
 
-public class MainActivity extends ActionBarActivity {
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    public Button buttonbasla;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.fragment_main);
+
+        buttonbasla = (Button) findViewById(R.id.buttonBasla);
+
+        //Listening to button event
+        buttonbasla.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                //Starting a new Intent
+                Intent nextScreen = new Intent(getApplicationContext(), secimyapilan.class);
+
+                //Sending data to another Activity
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+                // starting new activity
+                startActivity(nextScreen);
+
+            }
+        });
+
+
+  //  @Override
+   // public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+     //   getMenuInflater().inflate(R.menu.main, menu);
+      //  return true;
+    //}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    //@Override
+    //public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+     //   int id = item.getItemId();
+      //  if (id == R.id.action_settings) {
+       //     return true;
+        //}
+        //return super.onOptionsItemSelected(item);
+    //}
 
+}
 }
---
+++ app/src/main/java/com/bebek/takip/secimyapilan.java
@@ -0,0 +1,15 @@
+package com.bebek.takip;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yasemin on 4/27/14.
+ */
+public class secimyapilan extends Activity{
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secimyapilan);
+    }
+}
