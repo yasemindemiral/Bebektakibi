8dfd01f09f779bd2e305f43a3979c0a6d9286b20
GIT Admin <yasmindemiral@gmail.com>
2014-04-30 23:17:26 +0300

    mail ok

--- app/src/main/java/com/bebek/takip/bebekodasi.java
+++ app/src/main/java/com/bebek/takip/bebekodasi.java
@@ -1,49 +1,107 @@
 package com.bebek.takip;
 
-import android.app.Activity;
+import java.util.Properties;
+import java.util.Random;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import android.os.Bundle;
-import android.util.Log;
+import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.Random;
-import com.bebek.takip.GMailSender;
-
 /**
- * Created by yasemin on 4/27/14.
+ * Created by yasemin on 4/30/14.
  */
+
+class GmailAuthenticator extends Authenticator{
+    String user;
+    String pw;
+    public GmailAuthenticator (String username, String password){
+        super();
+        this.user= username;
+        this.pw = password;
+
+    }
+    public PasswordAuthentication getPasswordAuthentication(){
+        return new PasswordAuthentication(user, pw);
+    }
+
+}
 public class bebekodasi extends Activity {
-   public Button buttontamam;
-    public EditText editeposta;
-    int str, rNumber;
-
-
-
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bebekodasi);
-        editeposta = (EditText) findViewById(R.id.editEposta);
-
-        buttontamam = (Button) findViewById(R.id.buttonTamam);
+        final EditText editOnayla = (EditText) findViewById(R.id.editOnayla);
+        final EditText editEposta = (EditText) findViewById(R.id.editEposta);
+        Button buttontamam = (Button) findViewById(R.id.buttonTamam);
         buttontamam.setOnClickListener(new View.OnClickListener() {
-            @Override
-          public void onClick(View v) {
+
+            public void onClick(View arg0) {
 
                 final Random numGen = new Random(); //Random sayı oluşturmak için olan kısım
-                rNumber = numGen.nextInt(1000);
-                //editeposta.setText(String.valueOf(rNumber));
-                //str=rNumber;
-                try {
-                    GMailSender sender = new GMailSender("applockermail@gmail.com" , "app192837");
-                    sender.sendMail("konu",Integer.toString(rNumber),"applockergmail.com", String.valueOf(editeposta.getText()));
+                final Integer rndnumber = numGen.nextInt(1000);
+                //txt.setText(String.valueOf(rNumber));
+                final String rNumber = rndnumber.toString();
+                editOnayla.setText(String.valueOf(rndnumber));
 
-                }
-                catch (Exception e) {
-                    Log.e("SendMail", e.getMessage());
-                }
+
+
+                final String username = "applockermail@gmail.com";
+                final String password = "app192837";
+                final String posta = editEposta.getText().toString();
+
+                Properties props = new Properties();
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.starttls.enable", "true");
+                props.put("mail.smtp.host", "smtp.gmail.com");
+                props.put("mail.smtp.port", "587");
+
+                Session session = Session.getInstance(props, new GmailAuthenticator(username, password));
+                final Message message = new MimeMessage(session);
+                Runnable r = new Runnable() {
+                    @Override
+                    public void run() {
+
+                        try {
+                           // message.setFrom(new InternetAddress("yasmindemiral@gmail.com"));
+                            message.setFrom(new InternetAddress(posta));
+                            message.setRecipients(Message.RecipientType.TO,
+                                  //  InternetAddress.parse("yasmindemiral@gmail.com"));
+                                    InternetAddress.parse(posta));
+                            message.setSubject("Kullanıcı Kodu");
+                           // message.setText("Test");
+                            message.setText(rNumber);
+                            Transport.send(message);
+
+                        } catch (AddressException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        } catch (MessagingException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+                Thread t = new Thread(r);
+                t.start();
+
 
             }
-      });
+        });
+
+
     }
+
+
 }
