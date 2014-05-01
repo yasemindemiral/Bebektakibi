906422daa4e02e91f51d6f0efabd0024f6a5e5b9
Nigar Kocaman <nigarkocaman@gmail.com>
2014-05-01 13:35:00 +0300

    mail

--- app/src/main/java/com/bebek/takip/MainActivity.java
+++ app/src/main/java/com/bebek/takip/MainActivity.java
@@ -25,33 +25,11 @@ public class MainActivity extends Activity {
             public void onClick(View arg0) {
                 //Starting a new Intent
                 Intent nextScreen = new Intent(getApplicationContext(), secimyapilan.class);
-
                 // starting new activity
                 startActivity(nextScreen);
 
             }
         });
 
-
-  //  @Override
-   // public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-     //   getMenuInflater().inflate(R.menu.main, menu);
-      //  return true;
-    //}
-
-    //@Override
-    //public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-     //   int id = item.getItemId();
-      //  if (id == R.id.action_settings) {
-       //     return true;
-        //}
-        //return super.onOptionsItemSelected(item);
-    //}
-
-}
+    }
 }
